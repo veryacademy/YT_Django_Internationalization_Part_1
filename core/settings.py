6cef190045331f50b7d45c756e4a9ffb834dddf0
veryacademy <veryacademymail@gmail.com>
2021-02-17 11:53:07 +0000

    nc

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lang',
+    'rosetta'
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path(_('admin/'), admin.site.urls),
-    path('', include('lang.urls', namespace='lang'))
 ]
 
 urlpatterns += i18n_patterns (
     path('', include('lang.urls', namespace='lang'))
 )
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        re_path(r'^rosetta/', include('rosetta.urls'))
+    ]
